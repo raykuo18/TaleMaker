072ce378d9228061abffa1779d3ad1080a311180
quantumcheng <quantumcheng@gmail.com>
2022-12-10 00:53:05 +0800

    a lot of bugs are waiting for you

--- talemaker/utils/user_state.py
+++ talemaker/utils/user_state.py
@@ -1,4 +1,21 @@
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
 from enum import Enum
+
+from utils.utils import str_contain_chinese, chinese_convert, message_obj, pick_a_sentence
+from models.models import visual_question_answering, image_captioning
+
+from ai_line_bot.views import ChatBot #i don't even know what i've just import
+
+# Setup LINE Bot api
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
 
 STATES = Enum(
     'states',(
@@ -38,8 +55,47 @@ class UserClass():
             user_data = self.get_user_data(user_id)
             if message_type == "sticker" and user_data['state']() != STATES.IDLE:
                 user_data['state'].change_state(STATES.IDLE)
-                
+                line_bot_api.reply_message(
+                    self.reply_token,
+                    message_obj(pick_a_sentence("afterleaving", "en"))
+                )
+            elif user_data['state'] == STATES.IDLE:
+                if message_type == "sticker":
+                    user_data['state'].change_state(STATES.INIT) 
+                    line_bot_api.reply_message(
+                        self.reply_token,
+                        message_obj(
+                            f"Hello {profile.display_name}, welcome to TaleMaker\n"\
+                            + f"You can send a image to start the app!"
+                        )   #still can't figure it out how to set the profile here
+                    )   
+                elif message_type != "sticker":
+                    line_bot_api.reply_message(
+                        self.reply_token,
+                        message_obj(pick_a_sentence("greeting", "en"))
+                    )
+
+                elif user_data['state'] == STATES.INIT:
+                    if message_type == "text":
+                        line_bot_api.reply_message(
+                            self.reply_token,
+                            message_obj(pick_a_sentence("beforeCaptioning", "en"))
+                        )
+                    elif message_type == "image":
+                        user_data['state'].change_state(STATES.VQA)
+                        ChatBot.save_and_caption(event=event, userId=user_data["id"])   
+
+                elif user_data['state'] == STATES.VQA:
+                    if message_type == "text":
+                        ChatBot.answer_the_question(event=event, userId=user_data["id"])
+                    elif message_type == "image":
+                        ChatBot.save_and_caption(event=event, userId=user_data['id'])
+                    #I think it will auto fix when you import back to view.py or use 'img_file' as event    
+
+            else:
+                pass
             # TODO: finish the logic
+
         else: # new user
             new_user_data = {
                 "id": user_id,
